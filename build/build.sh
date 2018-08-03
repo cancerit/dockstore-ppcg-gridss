e0a37dcc5f5ac32d3e0c9fa5bfa86789bf9f9814
Yaobo Xu <yx2@sanger.ac.uk>
2018-08-03 15:00:58 +0000

    test-3

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 apt-get update
+#
+# # set up java
+# add-apt-repository -y ppa:webupd8team/java
+# bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
+# bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections'
+# apt-get -yq update
+# apt-get -yq install --no-install-recommends oracle-java8-installer
+apt install -y openjdk-8-jre-headless
 
-# set up java
-add-apt-repository -y ppa:webupd8team/java
-bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
-bash -c '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections'
-apt-get -yq update
-apt-get -yq install --no-install-recommends oracle-java8-installer
-
-# install dependencies
-apt-get install -yq --no-install-recommends bwa r-base
+# install other dependencies
+apt-get install -yq bwa r-base
